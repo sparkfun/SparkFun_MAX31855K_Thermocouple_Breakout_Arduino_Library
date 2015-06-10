8a8ab3b229a5e729d04387342f9d49ad8808e275
Brent Wilkins <brent@SparkFun.com>
2015-06-10 17:51:41 -0600

    Added missing whitespace

--- src/SparkFunMAX31855k.cpp
+++ src/SparkFunMAX31855k.cpp
@@ -1,5 +1,6 @@
 /*************************** SparkFunMAX31855k.cpp *****************************
- * Copyright (c) 2015 SparkFun Electronics                                     * *                                                                             *
+ * Copyright (c) 2015 SparkFun Electronics                                     *
+ *                                                                             *
  * Permission is hereby granted, free of charge, to any person obtaining a     *
  * copy of this software and associated documentation files (the "Software"),  *
  * to deal in the Software without restriction, including without limitation   *
